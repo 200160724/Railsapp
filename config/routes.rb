111f7f00508c2bbe0aceeec7f6042a0124f75b14
Abhishek <aksingh5@ncsu.edu>
2016-09-17 09:07:11 -0400

    pushing changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   #root 'welcome#hello'
   Rails.application.routes.draw do
-    root 'application#hello'
+    #root 'application#hello'
   end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
